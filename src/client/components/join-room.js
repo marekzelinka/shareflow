6699b82402fa7a166b53396a5d0ded667b1bdf3b
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-06-30 14:00:19 +0200

    hook join room form up with formik

--- src/client/components/join-room.js
+++ src/client/components/join-room.js
@@ -5,40 +5,99 @@ import {
   FormLabel,
   Input,
   Button,
+  FormErrorMessage,
+  Icon,
 } from "@chakra-ui/react";
+import { Formik } from "formik";
+import { ExclamationCircleIcon, PlusCircleIcon } from "@heroicons/react/solid";
 
 export const JoinRoom = () => {
   return (
-    <Stack as="form" spacing={6}>
-      <FormControl>
-        <FormLabel fontSize="sm" fontWeight="medium" textColor="gray.700">
-          Room title
-        </FormLabel>
-        <Input
-          _focus={{
-            outline: "none",
-            borderColor: "purple.400",
-            ring: "1px",
-            ringColor: "purple.400",
-          }}
-        />
-      </FormControl>
-      <Box>
-        <Button
-          colorScheme="purple"
-          isFullWidth
-          fontWeight="medium"
-          fontSize="sm"
-          _focus={{
-            outline: "none",
-            ring: "2px",
-            ringOffset: "2px",
-            ringColor: "purple.400",
-          }}
-        >
-          Join room
-        </Button>
-      </Box>
-    </Stack>
+    <Formik
+      initialValues={{ id: "" }}
+      validate={(values) => {
+        const errors = {};
+        if (values.id.trim().length === 0) {
+          errors.id = "Required";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <Stack as="form" spacing={6} onSubmit={handleSubmit}>
+          <FormControl isInvalid={errors.id !== undefined && touched.id}>
+            <FormLabel fontSize="sm" fontWeight="medium" textColor="gray.700">
+              Room ID
+            </FormLabel>
+            <Input
+              _focus={{
+                outline: "none",
+                borderColor: "purple.400",
+                ring: "1px",
+                ringColor: "purple.400",
+              }}
+              name="id"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.id}
+            />
+            <FormErrorMessage>
+              <Icon
+                as={ExclamationCircleIcon}
+                mr={1.5}
+                width={5}
+                height={5}
+                flexShrink={0}
+                textColor="red.400"
+                aria-hidden="true"
+              />
+              {errors.id}
+            </FormErrorMessage>
+          </FormControl>
+          <Box>
+            <Button
+              type="submit"
+              colorScheme="purple"
+              isFullWidth
+              fontWeight="medium"
+              fontSize="sm"
+              _focus={{
+                outline: "none",
+                ring: "2px",
+                ringOffset: "2px",
+                ringColor: "purple.400",
+              }}
+              leftIcon={
+                <Icon
+                  as={PlusCircleIcon}
+                  width={5}
+                  height={5}
+                  textColor="purple.200"
+                  aria-hidden="true"
+                />
+              }
+              isLoading={isSubmitting}
+              loadingText="Joining room. Please wait..."
+            >
+              Join room
+            </Button>
+          </Box>
+        </Stack>
+      )}
+    </Formik>
   );
 };
