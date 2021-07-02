8801ce4667d80960d5187fa749a40ea7c366ea7e
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-07-02 13:59:38 +0200

    fix container padding

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,4 @@
-import { Divider, Heading, Link, Text } from "@chakra-ui/react";
+import { Box, Divider, Heading, Link, Text } from "@chakra-ui/react";
 import { Layout } from "client/components";
 
 const About = () => {
@@ -17,38 +17,40 @@ const About = () => {
         </Heading>
       </Layout.Header>
       <Layout.Content>
-        <Text fontSize="lg">
-          shareflow is a simple helper for your presentations, where you can
-          create a room, share the URL with the attendees, and collaborate
-        </Text>
-        <Heading
-          as="h2"
-          mt={6}
-          size="md"
-          fontWeight="medium"
-          textColor="gray.900"
-        >
-          Get your attendees involved
-        </Heading>
-        <Text mt={2}>
-          You can share usefull links and snippets with others. For code
-          snippets we support 8 programming languages.
-        </Text>
-        <Divider mt={6} />
-        <Heading
-          as="h2"
-          mt={6}
-          size="md"
-          fontWeight="medium"
-          textColor="gray.900"
-        >
-          Contact
-        </Heading>
-        <Text mt={2}>
-          shareflow has been created by Marek Zelinka. You can find him on
-          Twitter, <Link>@mzelinka17</Link>, or <Link>reach out via email</Link>
-          .
-        </Text>
+        <Box px={{ base: 4, sm: 0 }}>
+          <Text fontSize="lg">
+            shareflow is a simple helper for your presentations, where you can
+            create a room, share the URL with the attendees, and collaborate
+          </Text>
+          <Heading
+            as="h2"
+            mt={6}
+            size="md"
+            fontWeight="medium"
+            textColor="gray.900"
+          >
+            Get your attendees involved
+          </Heading>
+          <Text mt={2}>
+            You can share usefull links and snippets with others. For code
+            snippets we support 8 programming languages.
+          </Text>
+          <Divider mt={6} />
+          <Heading
+            as="h2"
+            mt={6}
+            size="md"
+            fontWeight="medium"
+            textColor="gray.900"
+          >
+            Contact
+          </Heading>
+          <Text mt={2}>
+            shareflow has been created by Marek Zelinka. You can find him on
+            Twitter, <Link>@mzelinka17</Link>, or{" "}
+            <Link>reach out via email</Link>.
+          </Text>
+        </Box>
       </Layout.Content>
     </Layout>
   );
