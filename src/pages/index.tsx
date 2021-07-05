52ffa4b859fc1dd00fc68143821ee61bc7c62e88
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-07-05 11:37:26 +0200

    change transition type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,9 +50,9 @@ const Home = () => (
           backgroundColor="white"
           shadow="base"
           rounded={{ sm: "lg" }}
-          initial={{ y: 20, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.15, ease: "easeOut" }}
+          initial={{ y: 40 }}
+          animate={{ y: 0 }}
+          transition={{ type: "spring", damping: 12, stiffness: 200 }}
         >
           <CreateRoom />
           <Stack mt={6} spacing={6}>
