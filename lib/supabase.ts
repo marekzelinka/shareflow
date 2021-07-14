d9bdff165e7edf10de801957157702458a14925c
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-07-14 16:30:10 +0200

    rename supabase env vars

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-if (supabaseUrl === undefined) {
+const PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+if (PUBLIC_SUPABASE_URL === undefined) {
   throw new Error("Define the NEXT_PUBLIC_SUPABASE_URL env var");
 }
 
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-if (supabaseAnonKey === undefined) {
+const PUBLIC_SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+if (PUBLIC_SUPABASE_ANON_KEY === undefined) {
   throw new Error("Define the NEXT_PUBLIC_SUPABASE_ANON_KEY env var");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  PUBLIC_SUPABASE_URL,
+  PUBLIC_SUPABASE_ANON_KEY
+);
